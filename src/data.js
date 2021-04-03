d27735dbcd78b41663d49e3f2d7cf4c902168889
wg253 <wg253@cornell.edu>
2021-04-03 14:28:59 -0400

    Updated credentials in data.js

--- src/data.js
+++ src/data.js
@@ -1,17 +1,21 @@
 var admin = require("firebase-admin");
 
 // Fetch the service account key JSON file contents
-var serviceAccount = require("path/to/serviceAccountKey.json");
+var serviceAccount = require("post-visualize-development-firebase-adminsdk-u9ze6-9475ef2e0c.json");
 
 // Initialize the app with a service account, granting admin privileges
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://databaseName.firebaseio.com"
+  databaseURL: "https://post-visualize-development.firebaseio.com/"
 });
 
 // As an admin, the app has access to read and write all data, regardless of Security Rules
 var db = admin.database();
-var ref = db.ref("restricted_access/secret_document");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-});
+
+// var plant = localStorage.getItem("plantName")
+
+var ref = db.ref(plant);
+
+// ref.once("value", function(snapshot) {
+//   console.log(snapshot.val());
+// });
